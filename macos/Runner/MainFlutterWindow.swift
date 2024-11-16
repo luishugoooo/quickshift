97fb3d3616772789158d4de214c2548edda858b0
Luis Abeler <la@abeler-online.de>
2024-11-16 20:56:15 +0100

    Add CocoaPods support and integrate bitsdojo_window_macos for macOS

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,6 +1,6 @@
 import Cocoa
 import FlutterMacOS
-
+import bitsdojo_window_macos // Add this line
 class MainFlutterWindow: BitsdojoWindow  {
   override func bitsdojo_window_configure() -> UInt {
   return BDW_CUSTOM_FRAME | BDW_HIDE_ON_STARTUP
