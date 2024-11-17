fc634080282d92f07a01316ba8eb537e72524bdc
Luis Abeler <la@abeler-online.de>
2024-11-17 16:57:35 +0100

    Add keyboard shortcut for closing tabs and implement CloseTabIntent

--- lib/widgets/tabs/title_tab_bar.dart
+++ lib/widgets/tabs/title_tab_bar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quickshift/icons/dynamic_icons.dart';
+import 'package:quickshift/models/intents/close_tab.dart';
 import 'package:quickshift/state/tabs.dart';
 import 'package:quickshift/widgets/tabs/title_tab.dart';
 
@@ -11,44 +13,52 @@ class TitleTabBar extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final tabs = ref.watch(tabsProvider);
     final currentTab = ref.watch(currentTabProvider);
-    return LayoutBuilder(builder: (context, constraints) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          SizedBox(
-            height: constraints.maxHeight * 0.75,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child:
-                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                ...tabs.map(
-                  (e) {
-                    return TitleTab(
-                        onTap: () => ref
-                            .read(currentTabProvider.notifier)
-                            .selectTab(e.id),
-                        title: e.server?.name ?? "Tab ${e.id}",
-                        icon: DynamicIcons.server,
-                        isSelected: currentTab.id == e.id);
-                  },
-                ),
-                IconButton(
-                  onPressed: () =>
-                      ref.read(tabsProvider.notifier).newTab(setCurrent: true),
-                  //splashColor: Colors.transparent,
-                  hoverColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  iconSize: 16,
-                  icon: Icon(
-                    DynamicIcons.add,
-                    size: 16,
-                  ),
-                )
-              ]),
+    return Shortcuts(
+      shortcuts:  {
+        const SingleActivator(LogicalKeyboardKey.keyW, control: true):
+            CloseTabIntent()
+      },
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(
+              height: constraints.maxHeight * 0.75,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ...tabs.map(
+                        (e) {
+                          return TitleTab(
+                              onTap: () => ref
+                                  .read(currentTabProvider.notifier)
+                                  .selectTab(e.id),
+                              title: e.server?.name ?? "Tab ${e.id + 1}",
+                              icon: DynamicIcons.server,
+                              isSelected: currentTab.id == e.id);
+                        },
+                      ),
+                      IconButton(
+                        onPressed: () => ref
+                            .read(tabsProvider.notifier)
+                            .newTab(setCurrent: true),
+                        //splashColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        iconSize: 16,
+                        icon: Icon(
+                          DynamicIcons.add,
+                          size: 16,
+                        ),
+                      )
+                    ]),
+              ),
             ),
-          ),
-        ],
-      );
-    });
+          ],
+        );
+      }),
+    );
   }
 }
